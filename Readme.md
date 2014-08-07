91618097851c4640ecea99adc2e7e0c73b4a136b
Utkarsh Shukla <utk.shukla@gmail.com>
2014-08-07 12:59:02 +0530

    Added the files

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# The ERP Lens Framework
+
+ERP Lens is an Open Source ERP Framework that can be used to create applications.
--- erplens/tools/sql.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-def drop_view_if_exists(cr, viewname):
-    cr.execute("DROP view IF EXISTS %s CASCADE" % (viewname,))
-    cr.commit()
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
